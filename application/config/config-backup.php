bcee38bf7814d483ca55e40ec42490a3156885e4
Sunil Sapkota <sunil.17530067@lagrandee.edu.np>
2021-09-20 23:03:44 +0545

    new update on Mon 20/09/2021

--- application/config/config-backup.php
+++ application/config/config-backup.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
- // $config['base_url'] = 'http://localhost/raceClient';
- $config['base_url'] = 'https://client.racegroups.com';
+ $config['base_url'] = 'http://localhost/raceClient';
+ // $config['base_url'] = 'https://client.racegroups.com';  
 
 /*
 |--------------------------------------------------------------------------
